59deb7e133bb9b19600bdc5f0e931f8efa8bab36
v.yemelianov <v.yemelianov@voverc.com>
2020-07-19 11:09:43 +0700

    update config

--- dev-up.sh
+++ dev-up.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Starts all the services passed as arguments
-exec docker-compose --project-name na-service -f docker-compose.yml up --build na-service
+exec docker-compose --project-name na -f docker-compose.yml up --build na-service
--- down.sh
+++ down.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Stop and remove containers, networks, images, and volumes all the services specified in descriptors
-exec docker-compose -p na-service -f docker-compose.yml down
+exec docker-compose -p na -f docker-compose.yml down
