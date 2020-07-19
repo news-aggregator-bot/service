cdc03a996849e377eb6cccaf02784947c04bb4ce
v.yemelianov <v.yemelianov@voverc.com>
2020-07-19 16:44:24 +0700

    update deploy scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 # set -x #echo on
-
-while getopts 'c:u:h:f:v:' opt; do
-	case $opt in
-		u) USER="$OPTARG" ;;
-		h) HOST="$OPTARG" ;;				
-		c) CERT="$OPTARG" ;;
-		f) FILE="$OPTARG" ;;		
-		v) VERSION="$OPTARG" ;;		
-		*) exit 1 ;;
-	esac
-done
-
-ssh -o StrictHostKeyChecking=no -i certs/$CERT -l $USER $HOST "/bin/bash --login -s" -- < scripts/swarm-update.sh "$FILE"
+ssh -o StrictHostKeyChecking=no -i ~/.ssh/$SSH_KEY_FILE -l $SSH_USER $SSH_HOST "/bin/bash --login -s" -- < scripts/swarm-update.sh "https://raw.githubusercontent.com/news-aggregator-bot/service/master/docker-compose.yml"
