ffcc6a561d19904bc132c85c40a7f8bd29d0caeb
v.yemelianov <v.yemelianov@voverc.com>
2020-07-19 15:44:10 +0700

    update na_service name

--- scripts/swarm-update.sh
+++ scripts/swarm-update.sh
@@ -2,7 +2,7 @@ wget -q $1 -O docker-stack-service.yml
 docker stack ls | grep -q na-service
 if [ $? -eq 0 ]; then
     echo "Updating only selected services"
-    docker service update --image docker.pkg.github.com/news-aggregator-bot/artifactory/vlad110kg.na-service --with-registry-auth na_na-service
+    docker service update --image docker.pkg.github.com/news-aggregator-bot/artifactory/vlad110kg.na-service --with-registry-auth na_service_na-service
 else
     echo "Deploying new na stack"
     docker stack deploy --compose-file docker-stack-service.yml --with-registry-auth na-service
