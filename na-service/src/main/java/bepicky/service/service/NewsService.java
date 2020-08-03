4e4b96e5ccbf0e0beac6e10ac82701c4aab009c2
v.yemelianov <v.yemelianov@voverc.com>
2020-08-03 18:03:22 +0700

    fresh news empty check

--- na-service/src/main/java/bepicky/service/service/NewsService.java
+++ na-service/src/main/java/bepicky/service/service/NewsService.java
@@ -1,8 +1,5 @@
 package bepicky.service.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import bepicky.service.domain.NewsSyncResult;
 import bepicky.service.domain.PageParsedData;
 import bepicky.service.entity.NewsNote;
@@ -10,6 +7,9 @@ import bepicky.service.entity.Source;
 import bepicky.service.entity.SourcePage;
 import bepicky.service.exception.SourceNotFoundException;
 import bepicky.service.web.parser.WebContentParser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Comparator;
 import java.util.HashSet;
@@ -35,14 +35,15 @@ public class NewsService implements INewsService {
     @Override
     public NewsSyncResult sync(String name) {
         Source source = sourceService.findByName(name).orElseThrow(SourceNotFoundException::new);
-        Set<NewsNote> savedNotes = new HashSet<>(newsNoteService.saveAll(syncSource(source)));
+        Set<NewsNote> freshNews = syncSource(source);
+        Set<NewsNote> savedNotes = freshNews.isEmpty() ? freshNews : new HashSet<>(newsNoteService.saveAll(freshNews));
         return NewsSyncResult.builder().newsNotes(savedNotes).build();
     }
 
     @Override
     public NewsSyncResult sync(SourcePage sourcePage) {
         Set<NewsNote> freshNews = readFreshNews(sourcePage);
-        Set<NewsNote> savedNotes = new HashSet<>(newsNoteService.saveAll(freshNews));
+        Set<NewsNote> savedNotes = freshNews.isEmpty() ? freshNews : new HashSet<>(newsNoteService.saveAll(freshNews));
         return NewsSyncResult.builder().newsNotes(savedNotes).build();
     }
 
