70026a8b0e763dc1193c0d2ceeae0283b4e25513
v.yemelianov <v.yemelianov@voverc.com>
2020-08-23 13:25:35 +0700

    added urls to clients

--- na-service/src/main/java/bepicky/service/client/NaBotClient.java
+++ na-service/src/main/java/bepicky/service/client/NaBotClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "bepicky-client-bot", configuration = FeignClientConfiguration.class)
+@FeignClient(name = "bepicky-client-bot", url = "na-client-bot:8085", configuration = FeignClientConfiguration.class)
 public interface NaBotClient {
 
     @PutMapping("/notify-reader")
