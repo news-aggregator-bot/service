8347b275eb0640ede82b658fc55cd9f36d27376c
v.yemelianov <v.yemelianov@voverc.com>
2020-09-11 11:14:18 +0700

    updated sql script

--- na-service/src/main/resources/db/migration/V1.2__drop_description.sql
+++ na-service/src/main/resources/db/migration/V1.2__drop_description.sql
@@ -1,2 +1,4 @@
-alter table news_note drop column description;
+START TRANSACTION;
+delete from content_block_tag where id_tag in (select id from content_tag where `type` = 'DESCRIPTION');
 delete from content_tag where `type` = 'DESCRIPTION';
+commit;
