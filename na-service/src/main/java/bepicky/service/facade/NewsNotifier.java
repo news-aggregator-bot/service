6b77422ab4ae90d559c0d89bd9495be4258153c2
v.yemelianov <v.yemelianov@voverc.com>
2020-11-15 20:21:44 +0700

    added notify limit to configuration and made it configurable in na-service

--- na-service/src/main/java/bepicky/service/facade/NewsNotifier.java
+++ na-service/src/main/java/bepicky/service/facade/NewsNotifier.java
@@ -3,6 +3,7 @@ package bepicky.service.facade;
 import bepicky.common.domain.request.NotifyNewsRequest;
 import bepicky.service.client.NaBotClient;
 import bepicky.service.domain.mapper.NewsNoteDtoMapper;
+import bepicky.service.entity.NewsNote;
 import bepicky.service.entity.Reader;
 import bepicky.service.service.IReaderService;
 import lombok.extern.slf4j.Slf4j;
@@ -14,6 +15,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Component
 @Slf4j
@@ -48,8 +51,11 @@ public class NewsNotifier {
     }
 
     private void notify(Reader r) {
-        r.getNotifyQueue().stream()
+        Set<NewsNote> notes = r.getNotifyQueue().stream()
             .limit(notifyLimit)
+            .collect(Collectors.toSet());
+        notes
+            .stream()
             .map(n -> newsNoteDtoMapper.toDto(n, r.getPrimaryLanguage()))
             .map(request -> new NotifyNewsRequest(
                 r.getChatId(),
