8661d613dec9b33f4eafce1b090d51b585cea37e
v.yemelianov <v.yemelianov@voverc.com>
2021-01-02 19:07:43 +0700

    chown root root for driver

--- dev-up.sh
+++ dev-up.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-# Starts all the services passed as arguments
-docker-compose --project-name na -f docker-compose.yml -f docker-compose.dev.yml up --build --no-start na-service
-docker stack deploy --compose-file docker-compose.yml --compose-file docker-compose.dev.yml --with-registry-auth na-service
+docker stack deploy --compose-file docker-compose.yml --with-registry-auth na-service
